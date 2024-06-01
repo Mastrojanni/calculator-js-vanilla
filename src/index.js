84fc52be7e8b81598028c9d4f5bf8973b36f9c20
Mastrojanni <agrassi2135n@gmail.com>
2024-06-01 18:00:16 +0200

    fix: css specificity

--- src/index.js
+++ src/index.js
@@ -1,7 +1,41 @@
 "use strict"
 
+const BUTTONS_BASE_CLASSNAME = "b-";
+
+let expression = [];
+
+const calculatorMap = {
+	
+}
+
+const generateButtonClassName = (number) => {
+	return `${BUTTONS_BASE_CLASSNAME}${number}`;
+};
+
+const generateButtonsClickHanlders = () => {};
+
+const initButtonsHandlers = () => {
+
+	const buttons = [
+		generateButtonClassName(0),
+		generateButtonClassName(1),
+		generateButtonClassName(2),
+		generateButtonClassName(3),
+		generateButtonClassName(4),
+		generateButtonClassName(5),
+		generateButtonClassName(6),
+		generateButtonClassName(7),
+		generateButtonClassName(8),
+		generateButtonClassName(9),
+	];
+};
+
 const main = () => {
-	// alert("JS alive!");
+
+	initButtonsHandlers();
+	const id = generateButtonClassName(1);
+	const element = document.getElementById(id);
+	// element.onclick = () => {console.log("click 1")};
 };
 
 main();
