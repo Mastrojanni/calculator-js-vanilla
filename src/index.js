24bcd1ba60bca2125319a0e0d85b263154e3187b
Mastrojanni <agrassi2135n@gmail.com>
2024-06-01 19:34:11 +0200

    feat: first output area render version

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 "use strict"
 
+const OUTPUT_AREA_CLASSNAME = ".output-area";
 const CTA_AREA_CLASSNAME = ".action-buttons-area";
 const DATA_CTA_NUMBER_KEY = "ctatype";
 const DATA_CTA_NUMBER_VALUE = "number";
+
+let calculationBuffer = "";
+
+const updateOutputArea = (actionValueChoose) => {
+
+	calculationBuffer = `${calculationBuffer} ${actionValueChoose}`;
+
+	const outputAreaChildren = [...(document.querySelector(OUTPUT_AREA_CLASSNAME)).children];
+
+	outputAreaChildren[0].innerHTML = calculationBuffer;
+};
 
 const main = () => {
 
@@ -11,6 +23,11 @@ const main = () => {
 
 	const numberButtons = actionButtons.filter(button => (
 		button.dataset[DATA_CTA_NUMBER_KEY] == DATA_CTA_NUMBER_VALUE
+	));
+
+	// for each number button append its own number value to output buffer
+	numberButtons.forEach(numberElement => (
+		numberElement.onclick = () => updateOutputArea(numberElement.innerHTML)
 	));
 };
 
